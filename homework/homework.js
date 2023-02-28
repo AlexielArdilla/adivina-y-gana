5b72e64a772ad4f7ada515b59fa0a7d0b6ac29b5
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-02-28 15:44:45 -0300

    Volviendo a la modalidad con alerts

--- homework/homework.js
+++ homework/homework.js
@@ -6,59 +6,30 @@ const juego = () => {
 
     const numAleatorio = Math.floor((Math.random() * 10) + 1);
     console.log(numAleatorio);
+
+    const numElegido = prompt(`Adivine el núm del 1 al 10 tiene ${cantidadDeIntentos} intentos más!`);
+
+    if (numElegido < numAleatorio) {
+
+        cantidadDeIntentos--;
+
+        alert(`El número elegido ${numElegido} es menor al número a adivinar. 
+        
+        Cantidad de intentos restantes ${cantidadDeIntentos}`);
     
-        //const numElegido = prompt(`Adivine el núm del 1 al 10 tiene ${cantidadDeIntentos} intentos más!`);
+    } else if (numElegido > numAleatorio) {
 
-        const numElegido = myFunction();
-
-        escribe("Habla marica!!!");
-
-        if (numElegido > numAleatorio) {
-
-            cantidadDeIntentos--;
-
-            /*alert(`El número elegido ${numElegido} es menor al número a  adivinar Cantidad de
+        alert(`El número elegido ${numElegido} es mayor al número a  adivinar 
         
-        intentos restantes ${cantidadDeIntentos}`);*/
-            
-        myFunction(`El número elegido ${numElegido} es mayor al número a  adivinar Cantidad de
-        
-            intentos restantes ${cantidadDeIntentos}`);
+        Cantidad de intentos restantes ${cantidadDeIntentos}`);
 
+    } else {
 
-        } else if (numElegido < numAleatorio) {
+        alert("Acertó, es un verdadero genio!!!");
 
-            cantidadDeIntentos--;
+    }
 
-            /*alert(`El número elegido ${numElegido} es mayor al número a  adivinar de
-        
-        intentos restantes ${cantidadDeIntentos}`);*/
+};
 
-        myFunction(`El número elegido ${numElegido} es menor al número a  adivinar Cantidad de
-        intentos restantes ${cantidadDeIntentos}`);
+alert("Game over");
 
-        } else {
-
-            myFunction("Acertó, es un verdadero genio!!!");
-
-        }
-
-    }; 
-
-    myFunction("Game over");
-
-function myFunction() {
-
-    var x = document.getElementById("num").value;
-
-    document.getElementById("parrafo").innerHTML = x;
-
-    return x;
-
-}
-
-function escribe(texto){
-
-    document.getElementById("parrafo").innerHTML = texto;
-
-}
