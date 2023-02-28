5851b08fb19860e447bd07ae1995d270a9b5232c
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-02-28 06:19:24 -0300

    avanzado el juego hasta lógica

--- homework/homework.js
+++ homework/homework.js
@@ -1,1 +1,44 @@
 //Escriba aquí su código JavaScript:
+const juego = () => {
+    alert("Empieza el juego!!!");
+
+    let cantidadDeIntentos = 3;
+
+    const numAleatorio = Math.floor((Math.random() * 3) + 1);
+    console.log(numAleatorio);
+
+    do {
+
+        const numElegido = prompt(`Adivine el núm del 1 al 10 tiene ${cantidadDeIntentos} intentos más!`);
+
+        if (numElegido > numAleatorio) {
+
+            cantidadDeIntentos--;
+
+            alert(`El número elegido ${numElegido} es menor al número a  adivinar Cantidad de
+        
+        intentos restantes ${cantidadDeIntentos}`);
+
+
+
+        } else if (numElegido < numAleatorio) {
+
+            cantidadDeIntentos--;
+
+            alert(`El número elegido ${numElegido} es mayor al número a  adivinar de
+        
+        intentos restantes ${cantidadDeIntentos}`);
+
+        } else {
+
+            alert("Acertó, es un verdadero genio!!!");
+
+            break;
+
+        }
+
+    } while (cantidadDeIntentos > 0);
+
+    alert("Game over");
+
+}
